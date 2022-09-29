4c409101838f779bec85e3154cbc3c68f5fe5254
rahulurwa <rahulurwa97@gmail.com>
2022-09-29 22:44:39 +0530

    javascript

---
+++ app.js
@@ -0,0 +1,106 @@
+// console.log("hello world");
+// alert("iam a alert method");
+// prompt("Enter your age");
+// confirm("are you adult?");
+// document.write("iam from DOM");
+
+
+// var
+// var a;            //var declaration
+// console.log(a);   //undefined
+
+// a = 1;            //var initialization
+//  console.log(a);  //1
+
+// var a;            //var re-declaration
+// a = 100;          //var re-initialization
+// console.log(a);   //100
+
+// var a = 200;      //var declaration and initialization
+// console.log(a);   //200
+
+
+// let
+// let a;
+// console.log(a);     //undefined
+
+// a = 10;
+// console.log(a);     //10
+
+// let a;
+// a = 100;
+// console.log(a);     //100
+
+// let a = 20;
+// console.log(a);     //20
+
+// let a = 100;        //100
+
+
+// const
+// const a = 10;
+// console.log(a);
+
+
+
+// variable space & execution space
+// console.log(a);
+// var a = 5;
+// console.log(a);
+
+
+// let n = 100;
+// console.log(n);
+
+
+// console.log("start");
+// console.log(p);
+// var p = "hi";
+// console.log(p);
+// var q = "hello";
+// console.log(q);
+// // console.log(r);
+// let r = 50;
+// console.log(r);
+// console.log("end");
+
+
+
+// operators
+
+// var x = 2;
+// var y = 3;
+// console.log(x * y);
+
+// var x = 10;
+// // console.log(++10);
+// console.log(++x);
+
+
+
+
+// temporal dead zone
+
+// console.log(a);
+// var a = 10;
+
+// console.log(b);
+// let b = 20;
+
+// let b;
+// console.log(b);
+
+// console.log(c);
+// const c = 30;
+
+// const c=10;
+// console.log(c);
+
+
+
+
+
+
+
+
+
---
+++ day2.js
@@ -0,0 +1,167 @@
+//  addition operators:
+
+// let x = 11;
+// let y = 4;
+// console.log(x + y);
+
+// let a = "hi";
+// let b = "hello";
+// console.log(a + b);
+// console.log(x + a);
+// console.log(10 + "10");
+// console.log(10 + "5");
+
+
+// subtraction operators:
+
+// console.log(x - y);
+// console.log(x - a);
+// console.log(10 - "test");
+
+
+// multiplication operators
+
+// console.log(x * y);
+// console.log(x * a);
+// console.log(x * 0.5);
+// console.log(10 * "5");         //type casting will happen
+
+// division operators
+
+// console.log(x / y);            //type casting will happen
+// console.log(x / "hi");
+
+// modulus
+
+// console.log(x % y);
+// console.log(10 % 5);           //type casting will happen
+
+// Exponential operator (* *)
+
+// console.log(2 ** 2);
+// console.log(2 ** "2");         //type casting will happen
+// console.log(2 ** "hi");
+
+
+// increment operator (++)
+
+// console.log(++x);
+// // console.log(++10);
+// console.log(x);
+// console.log(++x);
+// console.log(x);
+// console.log(x++);
+// console.log(x);
+
+// decrement operator
+
+// console.log(--x);
+// console.log(x);
+// console.log(x--);
+// console.log(x);
+
+
+// assignment operator
+
+// let x = 10;
+// let y = 5;
+
+// x += 5;
+// console.log(x);
+
+// x -= 5;
+// console.log(x);
+
+// x *= 5;
+// console.log(x);
+
+// x /= 5;
+// console.log(x);
+
+// x %= 5;
+// console.log(x);
+
+
+// let str1 = "hello";
+// str1 += "world";
+// console.log(str1);
+
+// str1 -= "5";
+// console.log(str1);
+
+// let a = 10;
+// a -= "5";
+// console.log(a);
+// a *= "2";           // a = a "5" = 10 - 5=5; typecasting value of a will be 5  updated
+// console.log(a);
+
+
+
+// comparison operator
+
+// let x = 10;
+// let y = 10;
+// let z = "10";
+// console.log(x == y);
+// console.log(x === z);
+// console.log(x !== y);
+// console.log(x != y);
+// console.log(x != z);
+// console.log(x !== z);
+
+// ternary operator
+// 10 > 5 ? alert("yes") : alert("no");
+
+// if (10<5) {
+//     alert("yes");
+// }
+// else {
+//     alert("no");
+// }
+
+
+// let age = prompt("enter your age");
+// age > 18 ? alert("you are adult , you are allowed to use website"):
+// alert ("hey kid go and see pogo");
+
+
+// let a = Number(prompt("enter a value"));
+// let b = Number(prompt("enter a value"));
+// alert(a+b);
+
+
+// logical operators
+// logical AND(&&);
+
+// let a = 100;
+// let b = 50;
+// let c = 20;
+// alert(a > b && a < b);
+
+// logical OR(||)
+// alert(a > b || a < b);
+
+// logical NOT(!)
+// alert(!(a > b));
+
+
+// console.log(a > b || a < c || a < b);
+// console.log(a > b && a > c && a < b);
+// console.log(a > b && a > c || a < b);
+// console.log(a > b || (a > c && a < b));
+
+
+
+//  let n1 = prompt("enter n1");
+//  let n2 = prompt("enter n2");
+
+// let n1 = parseInt(prompt("enter n1"));
+// let n2 = parseInt(prompt("enter n2"));
+
+// console.log(n1 + n2);
+// console.log(n1 + n2);
+// console.log(n1 - n2);
+// console.log(n1 / n2);
+// console.log(n1 % n2);
+
+
